cf2948d91bb4128cbd49758b9073043c87d03ecb
timofei.netsvetaev <zappazoid@gmail.com>
2019-08-22 16:56:49 +0700

    Завершение логики скрипта. Добавление автоматора

--- KeySplitter.py
+++ KeySplitter.py
@@ -1,6 +1,7 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 # !/usr/local/bin/python3
 import sys
+import os
 
 
 def delete_keys_from_original(key_count, lines, current_file):
@@ -33,26 +34,58 @@ def get_file_strings(open_filename):
     return lines
 
 
+def get_count_from_filename(file_name):
+    sep = " "
+
+    fix_name = file_name.replace("—", sep).replace("\xc2\xa0", sep).replace(".txt", "")
+    fix_name = sep.join(fix_name.split())
+
+    word_list = fix_name.split(sep)
+
+    for word in word_list:
+        if word.isdigit() and len(word) > 1:
+            return int(word)
+
+
+def replace_key_count(file_name, count):
+    file_name = file_name.replace(".txt", "")
+    sep = " "
+
+    word_list = file_name.split(sep)
+    print (word_list)
+    for i in range(len(word_list)):
+        if word_list[i].isdigit() and len(word_list[i]) > 1:
+            word_list[i] = str(count)
+
+    ready_name = sep.join(word_list)
+    return ready_name + ".txt"
+
+
+def rename_original(old, new):
+    os.rename(old, new)
+
+
 def main(original_filename, key_count):
     key_count = int(key_count)
     # Получение всех ключей из файла
     lines = get_file_strings(original_filename)
 
-    # Создание пустого файла для новых ключей
-    folder_path = original_filename[0:original_filename.rfind("/") + 1]
-    new_filename = folder_path + "New Keys " + str(key_count)
-    new_file = open(new_filename + ".txt", "w")
+    current_key_count = int(get_count_from_filename(original_filename))
+    new_filename = replace_key_count(original_filename, key_count)
+    new_original_filename = replace_key_count(original_filename, current_key_count - key_count)
 
     # Записываем нужное кол-во ключей в новый файл
+    new_file = open(new_filename, "w")
     write_new_keys(key_count, lines, new_file)
 
     # Удаляем соответствующие строки в исходном файле (перезаписываем файл заново)
     delete_keys_from_original(key_count, lines, original_filename)
+    rename_original(original_filename, new_original_filename)
     return
 
 
 def test():
-    main("/Users/user/Downloads/TestKeys1400.txt", 50)
+    main("/Users/user/Downloads/Rules of The Mafia Free 1690.txt", 100)
 
 
 #test()
