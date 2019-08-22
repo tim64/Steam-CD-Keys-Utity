b8ba1715a55ba083b0cbbe006bc3023d300f4366
timofei.netsvetaev <zappazoid@gmail.com>
2019-08-22 13:49:12 +0700

    Фикс логики скрипта

--- KeySplitter.py
+++ KeySplitter.py
@@ -3,33 +3,60 @@
 import sys
 
 
-def main(open_filename, key_count):
-    f = open(open_filename)
-    lines = f.readlines()
-    all_keys_count = len(lines) - 1
-
-    folder_path = open_filename[0:open_filename.rfind("/") + 1]
-
+def delete_keys_from_original(key_count, lines, current_file):
     index = 0
-    new_name = folder_path + "New Keys " + str(key_count)
+    open_file = open(current_file, "w")
+    max_lines = len(lines)
+    for line in lines:
+        open_file.writelines(line)
+        index += 1
+        if index >= max_lines - key_count:
+            open_file.close()
+            return
 
-    new_f = open(new_name + ".txt", "w")
+
+def write_new_keys(key_count, lines, current_file):
+    index = 0
 
     for line in reversed(lines):
-        new_f.writelines(line)
+        current_file.writelines(line)
         index += 1
         if index >= key_count:
-            break
-
-    new_f.close()
-    f.close()
-
-    f = open(open_filename, "w")
-    for i in range(all_keys_count):
-        f.writelines(lines[i])
-        if i >= (int(all_keys_count) - int(key_count)):
-            f.close()
+            current_file.close()
             return
+
+
+def get_file_strings(open_filename):
+    open_file = open(open_filename)
+    lines = open_file.readlines()
+    open_file.close()
+    return lines
+
+
+def main(original_filename, key_count):
+    key_count = int(key_count)
+    # Получение всех ключей из файла
+    lines = get_file_strings(original_filename)
+
+    # Создание пустого файла для новых ключей
+    folder_path = original_filename[0:original_filename.rfind("/") + 1]
+    new_filename = folder_path + "New Keys " + str(key_count)
+    new_file = open(new_filename + ".txt", "w")
+
+    # Записываем нужное кол-во ключей в новый файл
+    write_new_keys(key_count, lines, new_file)
+
+    # Удаляем соответствующие строки в исходном файле (перезаписываем файл заново)
+    delete_keys_from_original(key_count, lines, original_filename)
+    return
+
+
+def test():
+    main("/Users/user/Downloads/TestKeys1400.txt", 50)
+
+
+#test()
+
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
