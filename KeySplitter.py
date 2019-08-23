3ff919f428fed788646a397db4cb8c542813ab24
timofei.netsvetaev <zappazoid@gmail.com>
2019-08-23 11:31:38 +0700

    Фиксы по ревью

--- KeySplitter.py
+++ KeySplitter.py
@@ -3,88 +3,104 @@
 import sys
 import os
 
+txt_ext = ".txt"  # extension
+space_char = " "
+empty_char = ""
+bad_spaces = "\xc2\xa0"  # non-breaking-space
 
-def delete_keys_from_original(key_count, lines, current_file):
-    index = 0
-    open_file = open(current_file, "w")
-    max_lines = len(lines)
-    for line in lines:
-        open_file.writelines(line)
-        index += 1
-        if index >= max_lines - key_count:
-            open_file.close()
+
+# Функция перезаписывает файл и записывает в него все ключ, кроме тех, которые мы отсекли ранее
+def delete_keys_from_original(key_count, key_list, filename):
+    f = open(filename, "w")
+    max_lines = len(key_list)
+    for index, key in enumerate(key_list):
+        if index == max_lines - key_count:
+            f.close()
             return
+        else:
+            f.writelines(key)
 
 
-def write_new_keys(key_count, lines, current_file):
-    index = 0
-
-    for line in reversed(lines):
-        current_file.writelines(line)
-        index += 1
-        if index >= key_count:
-            current_file.close()
+# Функция записывает ключи в новый файл
+def write_new_keys(key_count, key_list, filename):
+    f = open(filename, "w")
+    for index, key in enumerate(reversed(key_list)):
+        if index == key_count:
+            f.close()
             return
+        else:
+            f.writelines(key)
 
 
-def get_file_strings(open_filename):
-    open_file = open(open_filename)
-    lines = open_file.readlines()
-    open_file.close()
-    return lines
+# Функция проверяет доступность файла для чтения
+def check_file_read(filename):
+    try:
+        open(filename, 'rb')
+    except IOError:
+        print "Could not read file:", filename
+        return False
+
+    return True
 
 
-def get_count_from_filename(file_name):
-    sep = " "
-
-    fix_name = file_name.replace("—", sep).replace("\xc2\xa0", sep).replace(".txt", "")
-    fix_name = sep.join(fix_name.split())
-
-    word_list = fix_name.split(sep)
-
-    for word in word_list:
-        if word.isdigit() and len(word) > 1:
-            return int(word)
+# Функция получает список ключей из файла
+def get_keys_from_file(filename):
+    if check_file_read(filename):
+        f = open(filename)
+        keys = f.readlines()
+        f.close()
+        return keys
 
 
-def replace_key_count(file_name, count):
-    file_name = file_name.replace(".txt", "")
-    sep = " "
+# Функция очищает строку от лишних символов и возвращает лист из слов в строке
+def get_words_from_name(file_name):
+    fix_name = file_name.replace(bad_spaces, space_char)  # удаляем non-breaking пробелы из строки
+    fix_name = space_char.join(fix_name.split())  # удаляем лишние пробелы
 
-    word_list = file_name.split(sep)
+    word_list = fix_name.split(space_char)  # собираем список из слов
+    return word_list
+
+
+# Функция заменяет количество ключей в имени файла на новое
+def replace_key_count(file_name, key_count):
+    file_name = file_name.replace(txt_ext, empty_char)
+
+    word_list = file_name.split(space_char)
     for i in range(len(word_list)):
         if word_list[i].isdigit() and len(word_list[i]) > 1:
-            word_list[i] = str(count)
+            word_list[i] = str(key_count)
 
-    ready_name = sep.join(word_list)
-    return ready_name + ".txt"
+    ready_name = space_char.join(word_list)
+    return ready_name + txt_ext
 
 
-def rename_original(old, new):
-    os.rename(old, new)
+# Функция переименовывает файл
+def rename_original(old_name, new_name):
+    os.rename(old_name, new_name)
 
 
 def main(original_name, key_count):
-    key_count = int(key_count)
     # Получение всех ключей из файла
-    lines = get_file_strings(original_name)
+    keys = get_keys_from_file(original_name)
 
-    current_key_count = int(get_count_from_filename(original_name))
+    key_count = int(key_count)
+
+    current_key_count = len(keys)
+
     new_name = replace_key_count(original_name, key_count)
     new_original_name = replace_key_count(original_name, current_key_count - key_count)
 
     # Записываем нужное кол-во ключей в новый файл
-    new_file = open(new_name, "w")
-    write_new_keys(key_count, lines, new_file)
+    write_new_keys(key_count, keys, new_name)
 
     # Удаляем соответствующие строки в исходном файле (перезаписываем файл заново)
-    delete_keys_from_original(key_count, lines, original_name)
+    delete_keys_from_original(key_count, keys, original_name)
     rename_original(original_name, new_original_name)
     return
 
 
 def test():
-    main("/Users/user/Downloads/Rules of The Mafia Free 1690.txt", 100)
+    main("/Users/user/Downloads/Longest Monday 4400 Free — копия.txt", 100)
 
 
 #test()
